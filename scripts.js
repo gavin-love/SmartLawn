506254ce9179705f04a7043a06be885d84038d5b
gavin-love <1co.loco@oprotonmail.ch>
2018-10-07 16:35:41 -0600

    Add function to display all zones container once API calls are made
    Add function to identify all zone id's

--- scripts.js
+++ scripts.js
@@ -33,11 +33,26 @@ const displayZones = (zones) => {
         <p class="start_successful"></p>
         <form>
           <input type="text" placeholder="duration">seconds</input>
-          <button class="start">start</button>
+          <button class="start_one_zone">start</button>
         </form>
       </article>
     `)
   });
+};
+
+const displayStartAllZones = () => {
+  $('.start_all').empty();
+
+  $('.start_all').prepend(`
+    <article>
+      <p>Start All Zones</p>
+      <form>
+        <input class="duration" type="text" placeholder="Duration">seconds</input>
+        <input class="sort_order"type="text" placeholder="Sort Order" />
+        <button class="start_multiple_zones">Start</button>
+      </form>
+    </article>
+  `);
 };
 
 const fetchPerson = async () => {
@@ -61,6 +76,7 @@ const fetchPerson = async () => {
     const zones = user_data.devices[0].zones;
 
     displayUserInfo(user_data);
+    displayStartAllZones();
     displayZones(zones);
 
   } catch (err) {
@@ -68,7 +84,7 @@ const fetchPerson = async () => {
   }
 };
 
-const fetchZone = async (zone_id, zone_duration) => {
+const startZone = async (zone_id, zone_duration) => {
   const id = zone_id;
   const url = "https://api.rach.io/1/public/zone/start"
   const bearer_token = "76980330-8f0b-4659-a341-527364acf134";
@@ -95,14 +111,28 @@ const fetchZone = async (zone_id, zone_duration) => {
   }
 }
 
-const startZone = () => {
+const identifyZone = () => {
   event.preventDefault();
+
   const zone_id = $(event.target).parent().parent().attr('id');
   const zone_duration = parseInt($(event.target).siblings('input').val());
 
-  fetchZone(zone_id, zone_duration);
-}
+  startZone(zone_id, zone_duration);
+};
 
-$('.zones').on('click', 'article .start', startZone);
+const identifyAllZones = () => {
+  event.preventDefault();
+
+  const zone_duration = parseInt($(event.target).siblings('.duration').val());
+  const sort_order = parseInt($(event.target).siblings('.sort_order').val());
+  const zones = [];
+
+  $('.zones').find('article').each(function () { zones.push({ id: this.id, duration: zone_duration, sortOrder: sort_order }); });
+
+  console.log(zones);
+};
+
+$('.zones').on('click', 'article .start_one_zone', identifyZone);
+$('.start_all').on('click', 'article .start_multiple_zones', identifyAllZones)
 $(window).ready(fetchPerson);
 
