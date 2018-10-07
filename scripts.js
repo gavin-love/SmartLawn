a2b978413a2b8d8641bb002111567b2e7ea94e80
gavin-love <1co.loco@oprotonmail.ch>
2018-10-07 13:23:56 -0600

    Add event listener to start specific zones
    Add start zone function

--- scripts.js
+++ scripts.js
@@ -4,6 +4,8 @@ const milToStandard = (militaryTime) => {
 };
 
 const displayUserInfo = (user_data) => {
+  $('.user_info').empty();
+
   const dateWithTime = new Date();
   const date = dateWithTime.toJSON().slice(0, 10).replace(/-/g, '/');
   const militaryTime = dateWithTime.toJSON().slice(11, 19);
@@ -17,7 +19,27 @@ const displayUserInfo = (user_data) => {
   `);
 };
 
-const retrievePerson = async () => {
+const displayZones = (zones) => {
+  $('.zones').empty();
+
+  zones.map(zone => {
+    const nozzle = zone.customNozzle.name.replace(/_/g, ' ');
+
+    $('.zones').prepend(`
+      <article id="${zone.id}">
+        <h1>${zone.name}</h1>
+        <p>${nozzle}: ${zone.customNozzle.inchesPerHour}"/hr</p>
+        <img src="${zone.imageUrl}">
+        <form>
+          <input type="text" placeholder="duration">seconds</input>
+          <button class="start">start</button>
+        </form>
+      </article>
+    `)
+  });
+};
+
+const fetchPerson = async () => {
   $('.user_info').empty();
 
   const person_id = "2ee8a9ca-741d-4b1a-add3-8a7683e5aa28";
@@ -36,16 +58,30 @@ const retrievePerson = async () => {
     });
 
     const user_data = await response.json();
+    const zones = user_data.devices[0].zones;
+
     displayUserInfo(user_data);
+    displayZones(zones);
 
   } catch (err) {
     console.log(err)
   }
 };
 
-const retrieveZones = () => {
-  $('.zones_info').empty();
+const fetchZone = (zone_id) => {
+  const id = zone_id;
+  const url = "https://api.rach.io/1/public/zone/start"
+
+
 }
 
-$(window).ready(retrievePerson);
+const startZone = () => {
+  event.preventDefault();
+  const zone_id = $(event.target).parent().parent().attr('id');
+
+  fetchZone(zone_id);
+}
+
+$('.zones').on('click', 'article .start', startZone);
+$(window).ready(fetchPerson);
 
